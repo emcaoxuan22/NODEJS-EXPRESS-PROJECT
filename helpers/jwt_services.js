80d3529428fd98ddc89b4592b6e4e30bb073d015
“emcaoxuan22@gmail.com” <emcaoxuan22@gmail.com>
2023-09-18 15:26:37 +0700

    longcv

--- helpers/jwt_services.js
+++ helpers/jwt_services.js
@@ -1,5 +1,6 @@
 const createHttpError = require("http-errors");
 const JWT = require("jsonwebtoken");
+const redis = require("./connections_redis");
 require("dotenv").config();
 const signAccessToken = async (userId) => {
   return new Promise((resolve, reject) => {
@@ -24,52 +25,82 @@ const signRefreshToken = async (userId) => {
       if (err) {
         reject(err);
       }
-      resolve(token);
+      redis.set(
+        userId.toString(),
+        token,
+        "EX",
+        365 * 24 * 60 * 60,
+        (err, reply) => {
+          if (err) {
+            return reject(createHttpError.InternalServerError());
+          }
+          resolve(token);
+        }
+      );
     });
   });
 };
 
-const veryfiAccessToken =  (req, res, next) => {
-
-    if (!req.headers["authorization"]) {
-      throw createHttpError.Unauthorized();
-    }
-    const token = req.headers["authorization"].split(" ")[1];
-    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
-      if (err) {
-        if(err.name === "JsonwebTokenError"){
-          throw createHttpError.Unauthorized()
-        }
-        throw createHttpError.Unauthorized(err.message);
-      }
-      req.payload = payload;
-      next();
-    });
-    
-};
-
-const veryfiRefreshToken =  (req, res, next) => {
-
+const veryfiAccessToken = (req, res, next) => {
   if (!req.headers["authorization"]) {
     throw createHttpError.Unauthorized();
   }
   const token = req.headers["authorization"].split(" ")[1];
-  JWT.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, payload) => {
+  JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
     if (err) {
-      if(err.name === "JsonwebTokenError"){
-        throw createHttpError.Unauthorized()
+      if (err.name === "JsonwebTokenError") {
+        throw createHttpError.Unauthorized();
       }
       throw createHttpError.Unauthorized(err.message);
     }
     req.payload = payload;
     next();
   });
-  
+};
 
+const veryfiRefreshToken = async (req, res, next) => {
+  try {
+    if (!req.headers["authorization"]) {
+      throw createHttpError.Unauthorized();
+    }
+    const token = req.headers["authorization"].split(" ")[1];
+    JWT.verify(
+      token,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, payload) => {
+        try {
+          if (err) {
+            if (err.name === "JsonwebTokenError") {
+              throw createHttpError.Unauthorized();
+            }
+            throw createHttpError.Unauthorized(err.message);
+          }
+          await redis.get(payload.userId, async (err, reply) => {
+            try {
+              if (err) {
+                throw createHttpError.InternalServerError();
+              }
+              if (token != reply) {
+                throw createHttpError.InternalServerError();
+              }
+              req.payload = payload;
+              next();
+            } catch (error) {
+              next(error);
+            }
+          });
+        } catch (error) {
+          next(error);
+        }
+      }
+    );
+  } catch (error) {
+    next(error);
+  }
 };
 module.exports = {
   signAccessToken,
   veryfiAccessToken,
   signRefreshToken,
-  veryfiRefreshToken
+  veryfiRefreshToken,
 };
--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 async function check1() {
-        console.log("vao day khogn")
-        Promise.reject(new Promise((resolve, reject) => {
-            
-        }))
-        console.log("dau la gi",a)
-    
+  await new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     reject();
+    //   }, 5000);
+    reject();
+  });
+  console.log("qua buoc nay");
 }
 
 // async function check2() {
@@ -17,7 +18,17 @@ async function check1() {
 //     }
 //     console.log('xuong day')
 // }
-const b = check1().then(data => console.log('dasd', data)).catch(data => console.log(data))
+// const b = check1()
+//   .then((data) => console.log("vao cham then", data))
+//   .catch((data) => console.log("vao cham catch", data));
+// console.log("dxuong dy"),
+//   setTimeout(() => {
+//     console.log(b);
+//   }, 1000);
+
+const x = Promise.resolve(check1())
+  .then((data) => console.log("vaothen"))
+  .catch((data) => console.log("vao catch"));
 setTimeout(() => {
-    console.log(b)
-}, 1000);
+  console.log(x);
+}, 2000);
